f5cf2e76dd34a3ecf5b37d473936c74ffbcd67ff
Milend-UA <vitaliymilend@gmail.com>
2024-10-01 15:31:53 +0300

    Commit 1

---
+++ Test/script.js
@@ -0,0 +1,36 @@
+// let num = 111
+// let str = '222'
+// let result = num + str // получаем string
+//
+// console.log(result)
+// console.log(typeof result)
+
+/*
+консоли мы получаем:
+111222
+script.js:6 string
+ */
+
+// let userAge = prompt('How old r u?')
+// if (userAge >= 18) {
+//     console.log('Enter')
+// } else{
+//     console.log('No enter!!!')
+// }
+
+// console.log(`Age: ${userAge}`, 18)
+
+// let userAge = prompt('How old r u?')
+//
+// if (userAge<= 0) {
+//     console.log('Impossible')
+// } else{
+//     console.log(`Ur age is: ${userAge}`)
+// }
+
+let count = 0;
+
+do {
+    console.log(count);
+    count++ ;
+} while (count < 10) ;
